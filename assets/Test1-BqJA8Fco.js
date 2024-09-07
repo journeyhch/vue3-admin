33aa0af686d2824cd7c530c0135af1d9fb338d94
13212515612 <306772795@qq.com>
2024-09-07 15:48:08 +0800

    1

--- assets/Test1-BqJA8Fco.js
+++
@@ -1,1 +0,0 @@
-import{o,c,d as n,e as a,E as s,f as l}from"./index-_Od8McrU.js";const u={__name:"Test1",setup(r){function e(){console.log("m1")}return(_,m)=>{const t=s;return o(),c("div",null,[n(t,{type:"primary",onClick:e},{default:a(()=>[l("测试click")]),_:1})])}}};export{u as default};
